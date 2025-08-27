d81034e899aa2e65e161537a344515676db0d014
aftb777 <aftaabmulla228@gmail.com>
2025-08-27 12:29:18 +0530

    Initial Commit

---
+++ List and Scroll View/List_and_Scroll_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_Scroll_ViewApp.swift
+//  List and Scroll View
+//
+//  Created by Aftaab Mulla on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct List_and_Scroll_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ List and Scroll ViewTests/List_and_Scroll_ViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_Scroll_ViewTests.swift
+//  List and Scroll ViewTests
+//
+//  Created by Aftaab Mulla on 27/08/25.
+//
+
+import Testing
+@testable import List_and_Scroll_View
+
+struct List_and_Scroll_ViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
